7f228468f733b3557119dd1360ee6134a284c480
Patryk <patryk.podsiadlo@gmail.com>
2026-05-18 18:04:33 +0200

    [projekt] usuniecie zbednych plikow

--- world_dashboard/backend/test_llm.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from llm import summarize_article
-from main import db, models
-
-async def test():
-    res = await summarize_article("To jest testowy krótki artykuł o ataku zbrojnym na terytorium Ukrainy. Wojska rosyjskie wycofały się.", title="Testowy artykuł z Ukrainy")
-    print(res)
-
-asyncio.run(test())
